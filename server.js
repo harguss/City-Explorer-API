1285d71c6a4f56d890ca2bd7107f0fe48caa305f
SHargus <87950738+harguss@users.noreply.github.com>
2021-09-08 18:02:33 -0500

    connect to 3001

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 'use strict';
 
+const { response } = require('express');
 const express = require('express');
-
 const app = express();
+const  weather = require('./data/weather.json');
 
+console.log('weather', weather);
 const PORT = 3001;
+
+
+
+
+
+
+app.get("/", function (req, res) {
+  console.log("get /");
+  res.send('<h1>hello everyone</h1>');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log('Server started: https://localhost:${PORT}');
