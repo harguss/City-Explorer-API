855d6bfcbc61ef8464e5f490818379544c2a2719
SHargus <87950738+harguss@users.noreply.github.com>
2021-09-10 15:51:51 -0500

    loggig weather

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
-// const  weatherData = require('./data/weather.json');
-// console.log('weather', weatherData);
+//  const  weatherData = require('./data/weather.json');
+//  console.log('weather', weatherData);
 require('dotenv').config();
 const express = require('express');
 
@@ -14,66 +14,68 @@ const PORT = 3001;
 const axios = require('axios');
 
 
-//add axios require statement here.
+
 
 
 
 app.get('/', (req, res) => {
-  // console.log("get/weather");
+  //  console.log("get/weather");
   res.send('<h1>hello everyone</h1>');
 });
 
 
 app.get('/weatherData', getWeather); 
-
-
-
-// app.get('/weatherData', (request, response) => {
-//   // response.json(weatherData);
+//refactor is above
+//  app.get('/weatherData', (request, response) => {
+//  response.json(weatherData);
 // });
 
 
 
-// const response = await axios.get(url, {
-//   // query string parameters
-//   params: {
-//     // get key from environment variables
-//     key: process.env.REACT_APP_LOCATION_KEY,
-//     q, // variable already has correct name
-//     format: 'json',
-//   }
-// });
-// console.log(response);
 
 
 
 async function getWeather(request, response) {
-console.log('this is the req inside the getWeather',request)
-  // const q = request.query.q; // grab the q query string value
+// console.log('this is the req inside the getWeather',request.query)
+  const q = request.query.q; // grab the q query string value
 
-  // try {
-  //   const results = await axios.get('https://api.openweathermap.org/data/2.5/weather?q={q}&appid={}', {
-  //     params: {
-  //       //  process.env.WEATHER_API,
-       
-  //     },
-  //   });
+   try {
+     const results = await axios.get(`https://api.openweathermap.org/data/2.5/onecall`, {
+       params: {
+         appid: process.env.WEATHER_API_KEY,
+         lat:  41.9758872,
+         lon:  -91.6704053,
+       },
+     });
+    console.log('this is the results from the api' , results);
+    //  let getWeather =
+    //    results.data.map(weather => new Weather(weather));
 
-  //   let getWeather =
-  //     results.data.map(weather => new Weather(weather));
-
-  //   response.send(weatherArray);
-  // }
-  // catch (err) {
-  //   console.error('axios error!', err);
-  //   response.status(500).send('oops');
-  // }
+    //  response.send(weatherArray);
+   }
+   catch (err) {
+     console.error('axios error!', err);
+    //  response.status(500).send('oops');
+   }
 
 }
-//create async function to get the weather. 
+// create async function to get the weather. 
 
 
 //add blue code
+
+
+
+
+
+
+
+//New data gets sent through the class constructor function 
+// class Weather(){
+//   constructor(){}
+// }
+
+
 
 
 
